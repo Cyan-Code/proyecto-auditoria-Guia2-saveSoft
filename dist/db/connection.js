e5a5e192b2cfd2ebe8dc013804785424c0160d63
Luis Carrion <lcarrionalvarez@gmail.com>
2022-03-04 12:49:07 -0500

    Initialized tokens for help to register triggers

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -5,5 +5,15 @@ const db = new sequelize_1.Sequelize('auditoria', 'root', 'Mypoopsql.co!', {
     host: 'localhost',
     dialect: 'mysql'
 });
+/* db.query(
+  'DROP TRIGGER auditTrigger;'
+) */
+/* db.query(
+  'CREATE TRIGGER auditTrigger AFTER INSERT ON students' +
+  ' FOR EACH ROW' +
+  ' BEGIN' +
+  ' insert into audit (name, program, level, idStudent) values (new.name, new.program, new.level, new.id);' +
+  'END;'
+) */
 exports.default = db;
 //# sourceMappingURL=connection.js.map
